13a80c93bc50918daeb0eff0f613dd3734270d1b
Neilana <neilana@yandex.ru>
2017-02-27 00:30:06 +0300

    Small formating errors

--- task1/code/MyStack.cpp
+++ task1/code/MyStack.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <limits>
-#include <exception>    // std::exception
+#include <exception>	// std::exception
 #include <new>  
 
 // my headers
@@ -17,14 +17,14 @@ const string MyStack::DUMP_FILE_NAME = "dumpFile.txt";
 int MyStack::stacksCount = 0;
 
 /**
-*   Creates empty stack with given capacity. If capacity wasn't privided then capacity
-*   is equal to MyStack::DEFAULT_CAPACITY. Also assigns new id (MyStack::m_id) which is 
-*   equal to the number of already existing stacks.
+*	Creates empty stack with given capacity. If capacity wasn't privided then capacity
+*	is equal to MyStack::DEFAULT_CAPACITY. Also assigns new id (MyStack::m_id) which is 
+*	equal to the number of already existing stacks.
 *
-*   @param capacity - maximum number of elements in the stack.
+*	@param capacity - maximum number of elements in the stack.
 */
 MyStack::MyStack(MyStack::size_type capacity) : m_size(0), m_capacity(0), 
-		                        				m_id(stacksCount), m_data(NULL)
+												m_id(stacksCount), m_data(NULL)
 {
 	if (stacksCount == 0)
 		remove("dumpFile.txt");
@@ -39,22 +39,22 @@ MyStack::MyStack(MyStack::size_type capacity) : m_size(0), m_capacity(0),
 	} catch (exception& e)
 	{					 
 		string error = e.what();
-    	message += " Hey, sth is wrong! Exception caught: " + error + 
-	       		   ". (Input capacity:  " + to_string(capacity) + ")\n";
+		message += " Hey, sth is wrong! Exception caught: " + error + 
+					". (Input capacity:  " + to_string(capacity) + ")\n";
 	}
 	dump(message);
 }
 
 /**
-*   Creates new stack as a copy of other stack.
-*   
-*   @param obj - a reference to a stack that is being used to initialize new stack.  
+*	Creates new stack as a copy of other stack.
+*	
+*	@param obj - a reference to a stack that is being used to initialize new stack.  
 */
 MyStack::MyStack(const MyStack &obj)
 {
 	stacksCount++;
 	string message = "Creating stack from copy constructor (copy stack #" + 
-        			 to_string( obj.id() ) + ")... ";
+					 to_string( obj.id() ) + ")... ";
 
 	m_id = stacksCount;
 	m_size = obj.m_size;
@@ -76,8 +76,8 @@ MyStack::MyStack(const MyStack &obj)
 }
 
 /**
-*   Destroyes current stack. Frees allocated memory for stack elements. Writes result
-*   messages in the dump file.
+*	Destroyes current stack. Frees allocated memory for stack elements. Writes result
+*	messages in the dump file.
 */
 MyStack::~MyStack()
 {
@@ -96,13 +96,13 @@ MyStack::~MyStack()
 }
 
 /**
-*   Adds new value. In case of some problems writes messages in the dump file. Writes 
-*   error messages in the dump file.
-*   
-*   @param value - value to be added in the stack.
+*	Adds new value. In case of some problems writes messages in the dump file. Writes 
+*	error messages in the dump file.
+*	
+*	@param value - value to be added in the stack.
 *
-*   @return @c true - if value was added; 
-*   @return @c false - if value was not added. 
+*	@return @c true - if value was added; 
+*	@return @c false - if value was not added. 
 */
 bool MyStack::push(const value_type& value)
 {
@@ -111,10 +111,10 @@ bool MyStack::push(const value_type& value)
 	{
 		dump("Trying to push. Hmmmm... Something wrong with this stack! Capacity is 0 :(\n");
 	}
-	else	
+	else
 	if (m_size >= m_capacity)
 	{	
-		string message = "Trying to push to full stack (value: " + to_string(value) + ").\n";	
+		string message = "Trying to push to full stack (value: " + to_string(value) + ").\n";
 		dump(message);
 	}
 	else
@@ -124,48 +124,50 @@ bool MyStack::push(const value_type& value)
 }
 
 /**
-*   Returns and removes the top value from the stack. Writes error messages in the dump 
-*   file.
-*   
-*   @return @c result - the value of the top removed element.
+*	Returns and removes the top value from the stack. Writes error messages in the dump 
+*	file.
+*	
+*	@return @c result - the value of the top removed element.
 */
 MyStack::value_type MyStack::pop()
 {
 	value_type result = 0;	
 	
 	if ( m_size > 0  )
-    	result = m_data[--m_size];	
-    else
-    	dump("Trying to pop from empty stack!\n");
+		result = m_data[--m_size];	
+	else
+		dump("Trying to pop from empty stack!\n");
 
 	return result;
 }
 
 /**
-*   Returns reference ont the top value. Writes error messages in the dump file.
+*	Returns reference ont the top value. Writes error messages in the dump file.
 *
-*   @return @c result - the reference on the top element.
+*	@return @c result - the reference on the top element.
 */
 MyStack::value_type& MyStack::top()
 {
-    string message = "Trying top()... ";
+	string message = "Trying top()... ";
 	value_type *result = NULL;
 
 	if ( m_size > 0 )
-		result = &m_data[m_size-1],
-        message += "Success! Top element is: " + to_string(*result) +  ".\n"; 
+	{
+		result = &m_data[m_size-1];
+		message += "Success! Top element is: " + to_string(*result) +  ".\n"; 
+	}
 	else
-        message = "Stack is empty!\n", result = 0;
-    dump(message);
+		message = "Stack is empty!\n", result = 0;
+	dump(message);
 
 	return *result; 
 }
 
 /**
-*   Checks whether there's any elements in the stack.
+*	Checks whether there's any elements in the stack.
 *
-*   @return @c true - if stack is empty (no elements);
-*   @return @c false - if stack is not empty.
+*	@return @c true - if stack is empty (no elements);
+*	@return @c false - if stack is not empty.
 */
 bool MyStack::empty() const
 {
@@ -176,10 +178,10 @@ bool MyStack::empty() const
 }
 
 /**
-*   Check whether stack is valid or not.
+*	Check whether stack is valid or not.
 *
-*   @return @c true - stack is valid;
-*   @return @c false - stack is bad :(
+*	@return @c true - stack is valid;
+*	@return @c false - stack is bad :(
 */
 bool MyStack::ok() const
 {
@@ -187,27 +189,27 @@ bool MyStack::ok() const
 }
 
 /**
-*   Writes debug messages to the file. File name is determined by the 
-*   MyStack::DUMP_FILE_NAME variable. 
-*   
-*   @param message - message to write in the file.
+*	Writes debug messages to the file. File name is determined by the 
+*	MyStack::DUMP_FILE_NAME variable. 
+*	
+*	@param message - message to write in the file.
 */
 void MyStack::dump(const string &message) const
 {
-    // open dump file and write main info abot the stack
+	// open dump file and write main info abot the stack
 	ofstream dumpFile(DUMP_FILE_NAME, std::ios_base::app);
 	dumpFile << "Stack #" << m_id << endl;
 	dumpFile << "(Size: " << m_size << ", capacity: " << m_capacity << ")\n";
 
-    // write elements of the stack into a string
+	// write elements of the stack into a string
 	string elements = "";
 	if (m_size == 0) 
 		elements = "No elements.\n";
 	else
 		for (size_type i = 0; i < m_size; i++)
 			elements += "[" +to_string(i) + "] = " + to_string(m_data[i]) + "\n";
-    
-    // write elements and message to the file and close it
+	
+	// write elements and message to the file and close it
 	dumpFile << elements << message << endl;
 	dumpFile.close();
 }
@@ -216,17 +218,17 @@ MyStack& MyStack::operator=(MyStack &obj2)
 {
 	string message = "Assignment to stack #" + to_string( obj2.id() ) + "...\n";
 	dump(message);
-	if (this != &obj2)              // prevent self assignment
-	{      
-       	m_size = obj2.m_size;
-       	m_capacity = obj2.m_capacity;
-        if (m_data) 
-            delete[] m_data; 
-        m_data = new value_type [m_capacity];
-        for (MyStack::size_type i = 0; i < m_size; i++) 
-        	m_data[i] = obj2.m_data[i];
-    }
-	
+	if (this != &obj2)			  // prevent self assignment
+	{	  
+		m_size = obj2.m_size;
+		m_capacity = obj2.m_capacity;
+		if (m_data) 
+			delete[] m_data; 
+		m_data = new value_type [m_capacity];
+		for (MyStack::size_type i = 0; i < m_size; i++) 
+			m_data[i] = obj2.m_data[i];
+	}
+
 	dump("...assignment completed.\n");
 	return *this;
 }
--- task1/code/MyStack.h
+++ task1/code/MyStack.h
@@ -1,4 +1,4 @@
-///   @file MyStack.h
+///	@file MyStack.h
 
 // include guard 
 #pragma once
@@ -7,54 +7,54 @@
 #include <iostream>
 
 /**
-*   @class MyStack MyStack.h "MyStack.h"
-*   @brief MyStack class.
+*	@class MyStack MyStack.h "MyStack.h"
+*	@brief MyStack class.
 *
-*   My implementation of the stack structure.
+*	My implementation of the stack structure.
 */
 
 class MyStack
 {
 public:
-    // types definition
-	typedef float value_type;   ///< type of values stored in stack 
-    typedef size_t size_type;   ///< type of indexes used inside the array 
-    
-    // constructors and destructor			
-    MyStack(size_type capacity = DEFAULT_CAPACITY); ///< constructor
-	MyStack(const MyStack &obj);                    ///< copy constructor
-	~MyStack();                                     ///< destructor
-    
-    // main functions for stack
-	bool push (const value_type&  value);    ///< add value to the stack
-	value_type pop();                        ///< get and remove value from the stack
-	value_type& top();                       ///< access the top element 	
-	bool empty() const;                      ///< check if there's elements in the stack
+	// types definition
+	typedef float value_type;	///< type of values stored in stack 
+	typedef size_t size_type;	///< type of indexes used inside the array 
+	
+	// constructors and destructor
+	MyStack(size_type capacity = DEFAULT_CAPACITY); ///< constructor
+	MyStack(const MyStack &obj);					///< copy constructor
+	~MyStack();										///< destructor
+	
+	// main functions for stack
+	bool push (const value_type&  value);	///< add value to the stack
+	value_type pop();						///< get and remove value from the stack
+	value_type& top();						///< access the top element 	
+	bool empty() const;						///< check if there's elements in the stack
 
-    // operators overload
-	MyStack& operator=(MyStack &obj2);      ///< assignment operator overload
+	// operators overload
+	MyStack& operator=(MyStack &obj2);		///< assignment operator overload
 
-    // getters	
-    MyStack::size_type size() const;        ///< returns current number of elemetns in the stack
-	MyStack::size_type capacity() const;    ///< returns the maximum number of elements
-	MyStack::size_type id() const;          ///< returns id of the stack
+	// getters	
+	MyStack::size_type size() const;		///< returns current number of elemetns in the stack
+	MyStack::size_type capacity() const;	///< returns the maximum number of elements
+	MyStack::size_type id() const;			///< returns id of the stack
 
 private:
-    // static class members
-	static const size_type DEFAULT_CAPACITY = 6; ///< is used in constructor when capacity wasn't defined
-	static const std::string DUMP_FILE_NAME;     ///< name of the file where debug information is stored
-	static int stacksCount;                      ///< total amount of created stacks 
+	// static class members
+	static const size_type DEFAULT_CAPACITY = 6;///< is used in constructor when capacity wasn't defined
+	static const std::string DUMP_FILE_NAME;	///< name of the file where debug information is stored
+	static int stacksCount;						///< total amount of created stacks 
 
-    // information about the stack
-	size_type m_size;       ///< current number of elements in the stack
-	size_type m_capacity;   ///< maximum numbers of elements in the stack
-	size_type m_id;         ///< id of the stack 
+	// information about the stack
+	size_type m_size;		///< current number of elements in the stack
+	size_type m_capacity;	///< maximum numbers of elements in the stack
+	size_type m_id;			///< id of the stack 
 	
-	value_type *m_data;     ///< array of stack values 
+	value_type *m_data;		 ///< array of stack values 
 	
-    // check and debug functions
-	bool ok() const;                           ///< check if the stack is valid
-	void dump(const std::string &message = "" ) const; ///< write debug info in the file
+	// check and debug functions
+	bool ok() const;									///< check if the stack is valid
+	void dump(const std::string &message = "" ) const;	///< write debug info in the file
 };
 
 // inline functions implementation
