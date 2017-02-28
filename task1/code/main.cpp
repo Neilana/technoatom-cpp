47fb14df966bab358c754663ed963b647a665f5e
Oleg Ponomarev <onponomarev@gmail.com>
2017-03-01 01:20:58 +0400

    Reviewed main.cpp

--- task1/code/main.cpp
+++ task1/code/main.cpp
@@ -13,11 +13,22 @@
 // my headers
 #include "Stack.h"
 
+//FIXME: С макросами всегда надо быть осторожней. Представь себе, что у тебя был кусок кода:
+// if(a)
+//   MY_TEST(cond)
+// else {
+//   b();
+// }
+// Код под else будет выполнен совсем не тогда, когда ты этого ожидаешь.
+// Если кратко, то единственным универсальным решением будет убрать код макроса под do { } while (false)
+// в этом и только в этом случае MY_TEST(...) будет синтаксически эквивалентно выхову функции.
+// Подробнее об этом можно почитать тут http://stackoverflow.com/questions/154136/why-use-apparently-meaningless-do-while-and-if-else-statements-in-c-c-macros
 // simple unit-test
 #define MY_TEST( condition )  \
     if ( ! (condition) )    \
     cout << "Test failed: " << #condition << endl;  
 
+// Лучше сразу привыкай не использовать имена типа My***. Это перестает работать как только в проекте появляется больше 1 человека.
 using MyNamespace::Stack;
 using namespace std;
 
