5cb0ab0b1da4834bcdfe650d12af17e7a54ea2b3
penguinlav <mr.antlav@gmail.com>
2017-05-11 23:42:20 +0300

    Added move constructor, google style is coming
    Deleted double pro - file;
    cleared namespace

--- IlluminatiConfirmed/Exceptions/Exception.h
+++ IlluminatiConfirmed/Exceptions/Exception.h
@@ -3,74 +3,88 @@
 #include <string>
 #include <sstream>
 #include <vector>
-using std::string;
-using std::vector;
+#include <utility>
 
-#define EXCEPTION(message, ptr_on_Exception_or_nullptr) IlluminatiConfirmed::Exception(__LINE__, __FILE__, __PRETTY_FUNCTION__, message, ptr_on_Exception_or_nullptr );
+#define EXCEPTION(message, ptr_on_Exception_or_nullptr)                   \
+  IlluminatiConfirmed::Exception(__LINE__, __FILE__, __PRETTY_FUNCTION__, \
+                                 message, ptr_on_Exception_or_nullptr);
 
-namespace IlluminatiConfirmed
-{
-    class Exception
-    {
-        /*!
-         * \brief The ExceptionData struct Struct of data of exceptions
-         */
-        struct ExceptionData
-        {
-            ExceptionData() : line(0), file(""), func(""), message("") {}
-            ExceptionData(int l, string &&fu, string &&fi, string &&m) : line(l), file(fi), func(fu), message(m) {}
-            int line;
-            string file;
-            string func;
-            string message;
-        };
-    public:
+namespace IlluminatiConfirmed {
+class Exception {
+  /*!
+  * \brief The ExceptionData struct Struct of data of exceptions
+  */
+  struct ExceptionData {
+    ExceptionData() : line(0), file(""), func(""), message("") {}
+    ExceptionData(int l, std::string&& fu, std::string&& fi, std::string&& m)
+        : line(l),
+          file(std::move(fi)),
+          func(std::move(fu)),
+          message(std::move(m)) {}
+    int line;
+    std::string file;
+    std::string func;
+    std::string message;
+  };
 
-        /*!
-         * \brief Exception Forms a string with information about the error.
-         * \param code Error code
-         * \param mess Message
-         * \param line Line of code where it have happened
-         * \param file File of line where it have happened
-         * \param func Function of file where it have happened
-         */
-        Exception(int line, const char *file, const char *func, const char *message, const Exception *other = nullptr) : m_exceptionStorage(0)
-        {
-            if (other != nullptr)
-            {
-                m_exceptionStorage = other->m_exceptionStorage;
-            }
-            m_exceptionStorage.push_back(ExceptionData(line, string(file), string(func), string(message)));
-        }
+ public:
+  /*!
+  * \brief Exception Forms a std::string with information about the error.
+  * \param code Error code
+  * \param mess Message
+  * \param line Line of code where it have happened
+  * \param file File of line where it have happened
+  * \param func Function of file where it have happened
+  */
+  Exception(int line, std::string&& file, std::string&& func,
+            std::string&& message, const Exception* other = nullptr)
+      : m_exceptionStorage(0) {
+    if (other != nullptr) {
+      m_exceptionStorage = other->m_exceptionStorage;
+    }
+    m_exceptionStorage.push_back(ExceptionData(
+        line, std::move(file), std::move(func), std::move(message)));
+  }
 
-        /*!
-         * \brief what Returns the explanatory string.
-         * \return String
-         */
-        const string what() const
-        {
-            auto space = [](int j) -> const string
-            {
-                string sp = "";
-                while (j-- > 0) sp += "  ";
-                return sp;
-            };
-            std::stringstream os;
-            int j = 0;
-            for(auto it : m_exceptionStorage)
-            {
-                os<<std::noskipws<<space(j)<<"On line: "<<it.line<<".\n"<<space(j)<<"In file: "<<it.file<<".\n"<<space(j)<<"In func: "<<it.func<<".\n"<<space(j)<<"Message: "<<it.message<<"\n";
-                ++j;
-            }
-            return os.str();
-        }
-    private:
+  /*!
+   * \brief Exception Overloaded for lvalue message - std::string
+   */
+  Exception(int line, std::string&& file, std::string&& func,
+            const std::string& message, const Exception* other = nullptr)
+      : m_exceptionStorage(0) {
+    if (other != nullptr) {
+      m_exceptionStorage = other->m_exceptionStorage;
+    }
+    m_exceptionStorage.push_back(ExceptionData(
+        line, std::move(file), std::move(func), std::string(message)));
+  }
 
-        /*!
-         * \brief m_exceptionStorage Data of a exception keep here
-         */
-        vector<ExceptionData> m_exceptionStorage;
+  /*!
+  * \brief what Returns the explanatory std::string.
+  * \return std::string
+  */
+  const std::string what() const {
+    auto space = [](int j) {
+      std::string sp;
+      while (j-- > 0) sp += "  ";
+      return sp;
     };
+    std::stringstream os;
+    int j = 0;
+    for (auto it : m_exceptionStorage) {
+      os << std::noskipws << space(j) << "On line: " << it.line << ".\n"
+         << space(j) << "In file: " << it.file << ".\n"
+         << space(j) << "In func: " << it.func << ".\n"
+         << space(j) << "Message: " << it.message << "\n";
+      ++j;
+    }
+    return os.str();
+  }
+
+ private:
+  /*!
+  * \brief m_exceptionStorage Data of a exception keep here
+  */
+  std::vector<ExceptionData> m_exceptionStorage;
+};
 }
-
-
--- IlluminatiConfirmed/Exceptions/main.cpp
+++ IlluminatiConfirmed/Exceptions/main.cpp
@@ -1,58 +1,48 @@
 #include <iostream>
-#include "Exception.h"
 #include "..\Containers\Array\Array.h"
+#include "Exception.h"
 
 using std::string;
 using std::cout;
 using std::endl;
-string stage1_an_exception_is_thrown_here (IlluminatiConfirmed::Array<string, 3> &a1, int i);
-string stage2 (IlluminatiConfirmed::Array<string, 3> &a1);
-int main(int argc, char *argv[])
-{
-    (void) argc;
-    (void) argv;
-    {
-        IlluminatiConfirmed::Array<string, 3> simpleArray = {"Shla", "Sasha", "na"};
-        try
-        {
-            cout << stage2(simpleArray);
-        } catch (IlluminatiConfirmed::Exception &e)
-        {
-            cout << e.what();
-        }
-    }
+using namespace IlluminatiConfirmed;
 
-    return 0;
+string stage1_an_exception_is_thrown_here(Array<string, 3>& a1, unsigned int i);
+string stage2_replay_exception_from_stage1(Array<string, 3>& a1);
+int main(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+  {
+    Array<string, 3> simpleArray = {"Shla", "Sasha", "na"};
+    try {
+      cout << stage2_replay_exception_from_stage1(simpleArray);
+    } catch (Exception& e) {
+      cout << e.what();
+    }
+  }
+  return 0;
 }
 
-string stage1_an_exception_is_thrown_here (IlluminatiConfirmed::Array<string, 3>& a1,int i)
-{
-    string temp = "";
-    try
-    {
-        temp = a1.at(i);
-    } catch (IlluminatiConfirmed::Exception &e)
-    {
-        throw EXCEPTION("stage1", &e);
-    }
-    temp+= string(" ");
-    return temp;
+string stage1_an_exception_is_thrown_here(Array<string, 3>& a1,
+                                          unsigned int i) {
+  string temp;
+  try {
+    temp = a1.at(i);
+  } catch (Exception& e) {
+    throw EXCEPTION("stage1", &e);
+  }
+  temp += string(" ");
+  return temp;
 }
 
-string stage2 (IlluminatiConfirmed::Array<string, 3>& a1)
-{
-    string temp = "";
-
-    for (int i = 0; i < 4; ++i)
-    {
-        try
-        {
-            temp += stage1_an_exception_is_thrown_here(a1, i);
-        } catch (IlluminatiConfirmed::Exception &e)
-        {
-            throw EXCEPTION("stage2", &e);
-        }
+string stage2_replay_exception_from_stage1(Array<string, 3>& a1) {
+  string temp;
+  for (unsigned int i = 0; i < 4; ++i) {
+    try {
+      temp += stage1_an_exception_is_thrown_here(a1, i);
+    } catch (IlluminatiConfirmed::Exception& e) {
+      throw EXCEPTION("stage2", &e);
     }
-    return temp;
+  }
+  return temp;
 }
-
